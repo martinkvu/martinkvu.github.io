920e2a971d9d6eaa168c06628445addb11990ca0
Martin Vu <91714249+martinkvu@users.noreply.github.com>
2025-05-06 15:58:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", function () {
         typedTarget.textContent = ""; // Clear existing text
         new Typed("#typed", {
           stringsElement: "#typed-strings",
-          typeSpeed: 25,
+          typeSpeed: 5,
           backSpeed: 0,
           backDelay: 0,
           loop: false,
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function () {
       typedTarget.textContent = "";
       new Typed("#typed", {
         stringsElement: "#typed-strings",
-        typeSpeed: 25,
+        typeSpeed: 5,
         backSpeed: 0,
         backDelay: 0,
         loop: false,
