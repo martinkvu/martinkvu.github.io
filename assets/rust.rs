0b97f330251a6d66bb094b007174cb6329812eb5
Martin Vu <91714249+martinkvu@users.noreply.github.com>
2025-05-14 21:33:05 -0500

    Delete assets/rust.rs

--- assets/rust.rs
+++
@@ -1,7 +0,0 @@
-MartinVuWebsite/
-│── index.html   (Main webpage)
-│── styles.css   (CSS file for styling)
-│── script.js    (Optional JavaScript file)
-│── profile.jsx    (Optional React file)
-│── Images/      (Folder for images)
-│── assets/      (Additional resources)
